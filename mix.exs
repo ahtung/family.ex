16aa45865499f99d86c4b8bc013e47fdc5602b1a
Dunya Kirkali <dunya.kirkali@icemobile.com>
2018-04-06 22:57:51 +0200

    Prepare package description

--- mix.exs
+++ mix.exs
@@ -8,6 +8,8 @@ defmodule Family.Mixfile do
       elixir: "~> 1.3",
       start_permanent: Mix.env == :prod,
       deps: deps(),
+      package: package(),
+      description: description(),
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [coveralls: :test]
     ]
@@ -27,5 +29,22 @@ defmodule Family.Mixfile do
       {:ex_doc, ">= 0.0.0", only: :dev},
       {:credo, "~> 0.8.10", only: [:dev, :test], runtime: false}
     ]
+  end
+
+  # Package Information
+  defp package do
+    [
+      files: ["test", "lib", "mix.exs", "README.md", "LICENSE*"],
+      maintainers: ["Dunya Kirkali", "Onur Kucukkece"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/ahtung/family.ex"}
+    ]
+  end
+
+  # Package description
+  defp description do
+    """
+    No man has ever lived that had enough of children’s gratitude or woman’s love
+    """
   end
 end
