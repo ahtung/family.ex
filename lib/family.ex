625163c0ebc777de2c503ca0e6c91a00f7381e10
Dunya Kirkali <dunya.kirkali@icemobile.com>
2018-04-06 22:34:20 +0200

    Implement get_individual_name/2

--- lib/family.ex
+++ lib/family.ex
@@ -1,19 +1,33 @@
 defmodule Family do
   @moduledoc """
-  Documentation for Family.
+  Family module is to be used in order to parse GEDCOM 5.5 files.
   """
 
   @individual_tag "INDI"
 
   @doc """
-  Hello world.
+  Counts the number of individuals
   """
   def count_individuals(file_path) do
+    parse(file_path)
+    |> Enum.filter(fn(x) -> String.contains?(x, @individual_tag) end)
+    |> Enum.count
+  end
+
+  @doc """
+  Returns the name of an individual given an ID
+  """
+  def get_individual_name(file_path, individual_id) do
+    data = parse(file_path)
+    index = Enum.find_index(data, fn(x) -> String.contains?(x, "0 @#{individual_id}@ INDI") end)
+    name_row = Enum.at(data, index + 1)
+    Regex.run(~r/1 NAME (.*)/, name_row, [capture: :all_but_first]) |> List.first
+  end
+
+  defp parse(file_path) do
     {:ok, data} = File.read(file_path)
 
     data
     |> String.split("\n")
-    |> Enum.filter(fn(x) -> String.contains?(x, @individual_tag) end)
-    |> Enum.count
   end
 end
--- test/family_test.exs
+++ test/family_test.exs
@@ -2,7 +2,11 @@ defmodule FamilyTest do
   use ExUnit.Case
   doctest Family
 
-  test "count_individuals" do
+  test "count_individuals/1" do
     assert Family.count_individuals("priv/test.ged") == 3
+  end
+
+  test "get_individual_name/2" do
+    assert Family.get_individual_name("priv/test.ged", "I3") == "James /Smith/"
   end
 end
