09ffb74cc11145ae0c113b1efe490bebd7c9fc4f
Dunya Kirkali <dunya.kirkali@icemobile.com>
2018-04-06 22:44:42 +0200

    Add coveralls and credo

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule Family.Mixfile do
       version: "0.1.0",
       elixir: "~> 1.3",
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [coveralls: :test]
     ]
   end
 
@@ -21,8 +23,9 @@ defmodule Family.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:excoveralls, "~> 0.8.1", only: :test},
+      {:ex_doc, ">= 0.0.0", only: :dev},
+      {:credo, "~> 0.8.10", only: [:dev, :test], runtime: false}
     ]
   end
 end
