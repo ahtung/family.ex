8a5e81640b46e9badfa84e8744cfeb48c8ead110
Dunya Kirkali <dunya.kirkali@icemobile.com>
2018-04-06 22:12:05 +0200

    Implement Family.count_individuals/1

--- lib/family.ex
+++ lib/family.ex
@@ -3,16 +3,17 @@ defmodule Family do
   Documentation for Family.
   """
 
+  @individual_tag "INDI"
+
   @doc """
   Hello world.
-
-  ## Examples
-
-      iex> Family.hello
-      :world
-
   """
-  def hello do
-    :world
+  def count_individuals(file_path) do
+    {:ok, data} = File.read(file_path)
+
+    data
+    |> String.split("\n")
+    |> Enum.filter(fn(x) -> String.contains?(x, @individual_tag) end)
+    |> Enum.count
   end
 end
--- test/family_test.exs
+++ test/family_test.exs
@@ -2,7 +2,7 @@ defmodule FamilyTest do
   use ExUnit.Case
   doctest Family
 
-  test "greets the world" do
-    assert Family.hello() == :world
+  test "count_individuals" do
+    assert Family.count_individuals("priv/test.ged") == 3
   end
 end
