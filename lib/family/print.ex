1e5a176f738ccb842612739ecf443a4bd0f17b8c
Dunya Kirkali <dunyakirkali@gmail.com>
2018-05-03 09:06:46 +0200

    Improve speed

--- lib/family/print.ex
+++ lib/family/print.ex
@@ -8,24 +8,20 @@ defmodule Family.Print do
     file_path
     |> parse
     |> Enum.map(fn(row) ->
-      regex = ~r/(?<number>\d+) (?<tag>.+)(?<value> .+)?/
+      regex = ~r/^(?<level>0|[1-9]+[0-9]*) (?<pointer>@[^@]+@ |)(?<tag>[A-Za-z0-9_]+)(?<value> [^\n\r]*|)/
       captures = Regex.named_captures(regex, row)
       do_raw(captures)
     end)
   end
 
-  defp do_raw(%{"number" => number, "tag" => tag, "value" => value}) do
-    String.duplicate("  ", String.to_integer(number)) <> tag
+  defp do_raw(%{"level" => level, "pointer" => pointer, "tag" => tag, "value" => value}) do
+    String.duplicate("\t", String.to_integer(level)) <> pointer <> tag <> value
     |> IO.puts
-  end
-
-  defp do_print_raw(other) do
-    IO.puts "ERROR"
   end
 
   defp parse(file_path) do
     {:ok, data} = File.read(file_path)
 
-    data |> String.split("\n")
+    data |> String.split("\n", trim: true)
   end
 end
